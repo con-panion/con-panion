07b3374fef3830d099b5d1784f2cf78c57b91e52
Lahgolz <lahgolz@proton.me>
2024-05-16 00:11:43 +0200

    fix: register page tests

--- tests/browser/pages/auth/register.spec.ts
+++ tests/browser/pages/auth/register.spec.ts
@@ -1,3 +1,4 @@
+import hash from '@adonisjs/core/services/hash';
 import testUtils from '@adonisjs/core/services/test_utils';
 import { test } from '@japa/runner';
 
@@ -9,49 +10,55 @@ test.group('Auth register', (group) => {
 	test('Register page show register form', async ({ assert, visit, route }) => {
 		const page = await visit(route('auth.register'));
 
-		assert.snapshot(await page.innerHTML('body')).match();
+		await page.assertPath(route('auth.register'));
+
+		const body = await page.innerHTML('body');
+
+		assert.snapshot(body.trim()).match();
 	});
 
 	test('Submit button is not activated when the form is invalid', async ({ visit, route }) => {
 		const page = await visit(route('auth.register'));
+		const submitButton = page.getByRole('button', { name: 'Create an account' });
 
-		await page.assertDisabled('button[type="submit"]');
+		await page.assertDisabled(submitButton);
 
-		await page.fill('input[name="email"]', 'not-an-email');
-		await page.fill('input[name="password"]', 'password');
-		await page.fill('input[name="confirmPassword"]', 'not-the-same-password');
-		await page.assertDisabled('button[type="submit"]');
+		await page.getByLabel('Email').fill('not-an-email');
+		await page.getByLabel(/^Password$/).fill('password');
+		await page.getByLabel('Confirm password').fill('not-the-same-password');
+		await page.assertDisabled(submitButton);
 	});
 
 	test('Submit button is activated when the form is valid', async ({ visit, route }) => {
 		const page = await visit(route('auth.register'));
+		const submitButton = page.getByRole('button', { name: 'Create an account' });
 
-		await page.assertDisabled('button[type="submit"]');
+		await page.assertDisabled(submitButton);
 
-		await page.fill('input[name="email"]', 'test@test.fr');
-		await page.fill('input[name="password"]', 'Test123!');
-		await page.fill('input[name="confirmPassword"]', 'Test123!');
-		await page.assertNotDisabled('button[type="submit"]');
+		await page.getByLabel('Email').fill('test@test.fr');
+		await page.getByLabel(/^Password$/).fill('Test123!');
+		await page.getByLabel('Confirm password').fill('Test123!');
+		await page.assertNotDisabled(submitButton);
 	});
 
 	test('Show error message when form field is invalid', async ({ visit, route }) => {
 		const page = await visit(route('auth.register'));
 
-		await page.fill('input[name="email"]', 'not-an-email');
+		await page.getByLabel('Email').fill('not-an-email');
 		await page.assertVisible(page.getByText('The email field must be a valid email address'));
 
-		await page.fill('input[name="password"]', 'password');
+		await page.getByLabel(/^Password$/).fill('password');
 		await page.assertVisible(page.getByText('The password field format is invalid'));
 
-		await page.fill('input[name="confirmPassword"]', 'not-the-same-password');
+		await page.getByLabel('Confirm password').fill('not-the-same-password');
 		await page.assertVisible(page.getByText('The confirmPassword field and password field must be the same'));
 	});
 
 	test('Remove error message when form field is valid', async ({ visit, route }) => {
 		const page = await visit(route('auth.register'));
 
-		await page.fill('input[name="email"]', 'not-an-email');
-		await page.fill('input[name="email"]', 'test@test.fr');
+		await page.getByLabel('Email').fill('not-an-email');
+		await page.getByLabel('Email').fill('test@test.fr');
 		await page.assertNotVisible(page.getByText('The email field must be a valid email address'));
 	});
 
@@ -60,10 +67,10 @@ test.group('Auth register', (group) => {
 		const showPasswordButton = page.locator('input[name="password"] + svg');
 
 		await showPasswordButton.click();
-		assert.assert((await page.getAttribute('input[name="password"]', 'type')) === 'text');
+		assert.assert((await page.getByLabel(/^Password$/).getAttribute('type')) === 'text');
 
 		await showPasswordButton.click();
-		assert.assert((await page.getAttribute('input[name="password"]', 'type')) === 'password');
+		assert.assert((await page.getByLabel(/^Password$/).getAttribute('type')) === 'password');
 	});
 
 	test("Don't toggle password visibility when clicking on the eye icon for the other password field", async ({
@@ -76,61 +83,75 @@ test.group('Auth register', (group) => {
 		const showConfirmPasswordButton = page.locator('input[name="confirmPassword"] + svg');
 
 		await showPasswordButton.click();
-		assert.assert((await page.getAttribute('input[name="confirmPassword"]', 'type')) === 'password');
+		assert.assert((await page.getByLabel('Confirm password').getAttribute('type')) === 'password');
 		await showPasswordButton.click();
 
 		await showConfirmPasswordButton.click();
-		assert.assert((await page.getAttribute('input[name="password"]', 'type')) === 'password');
+		assert.assert((await page.getByLabel(/^Password$/).getAttribute('type')) === 'password');
 	});
 
 	test('Show error returned by the server when submitting form with existing email', async ({ visit, route }) => {
+		hash.fake();
+
+		const alreadyRegisteredUser = await User.create({ email: 'test@test.fr', password: 'Test123!' });
+
+		await alreadyRegisteredUser.save();
+
 		const page = await visit(route('auth.register'));
-		const alreadyRegisteredUsed = await User.create({ email: 'test@test.fr', password: 'Test123!' });
 
-		await alreadyRegisteredUsed.save();
-
-		await page.fill('input[name="email"]', alreadyRegisteredUsed.email);
-		await page.fill('input[name="password"]', alreadyRegisteredUsed.password);
-		await page.fill('input[name="confirmPassword"]', alreadyRegisteredUsed.password);
-		await page.click('button[type="submit"]');
+		await page.getByLabel('Email').fill(alreadyRegisteredUser.email);
+		await page.getByLabel(/^Password$/).fill('Test123!');
+		await page.getByLabel('Confirm password').fill('Test123!');
+		await page.getByRole('button', { name: 'Create an account' }).click();
 		await page.waitForSelector('.toast');
 
 		await page.assertVisible(page.getByText('The email has already been taken'));
 		await page.assertVisible(page.getByText('An error occurred while creating your account'));
+
+		hash.restore();
 	});
 
 	test('Show confirmation message returned by the server when submitting the form', async ({ visit, route }) => {
+		hash.fake();
+
 		const page = await visit(route('auth.register'));
 
-		await page.fill('input[name="email"]', 'test@test.fr');
-		await page.fill('input[name="password"]', 'Test123!');
-		await page.fill('input[name="confirmPassword"]', 'Test123!');
-		await page.click('button[type="submit"]');
+		await page.getByLabel('Email').fill('test@test.fr');
+		await page.getByLabel(/^Password$/).fill('Test123!');
+		await page.getByLabel('Confirm password').fill('Test123!');
+		await page.getByRole('button', { name: 'Create an account' }).click();
 		await page.waitForURL(route('home'));
 
 		await page.assertVisible(page.getByText('Account created successfully'));
+
+		hash.restore();
 	});
 
 	test('Home page show register button when not logged in', async ({ visit, route }) => {
 		const page = await visit(route('home'));
+		const registerLink = page.getByRole('link', { name: 'Register' });
 
-		await page.assertExists(`a[href="${route('auth.register')}"]`);
+		await page.assertExists(registerLink);
 
-		await page.click(`a[href="${route('auth.register')}"]`);
+		await registerLink.click();
 		await page.waitForURL(route('auth.register'));
 
 		await page.assertUrlContains(route('auth.register'));
 	});
 
 	test("Home page don't show register button when logged in", async ({ visit, route }) => {
+		hash.fake();
+
 		const page = await visit(route('auth.register'));
 
-		await page.fill('input[name="email"]', 'test@test.fr');
-		await page.fill('input[name="password"]', 'Test123!');
-		await page.fill('input[name="confirmPassword"]', 'Test123!');
-		await page.click('button[type="submit"]');
+		await page.getByLabel('Email').fill('test@test.fr');
+		await page.getByLabel(/^Password$/).fill('Test123!');
+		await page.getByLabel('Confirm password').fill('Test123!');
+		await page.getByRole('button', { name: 'Create an account' }).click();
 		await page.waitForURL(route('home'));
 
 		await page.assertNotExists(page.getByRole('link', { name: 'Register' }));
+
+		hash.restore();
 	});
 });
